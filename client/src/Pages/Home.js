f2431e64c6b6a7ef123c434a42e66f5101c92265
smullock <smullock@hotmail.com>
2023-05-09 21:53:38 +1000

    update homepage

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Defaultlayout from '../Components/Defaultlayout';
 import Header from '../Components/Header';
-import ItemFormModal from '../Components/ItemFormModal';
+
 import ItemList from '../Components/ItemList';
-import TripItems from '../Components/TripItems';
-import { GET_ITEMS } from '../utils/queries';
+
 
 
 
