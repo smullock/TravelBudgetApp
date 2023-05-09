29dcc3085b5246f22b677bee7307c237b1333cb9
smullock <smullock@hotmail.com>
2023-05-09 20:52:36 +1000

    'deploying'

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://smullock:1TaP54Gf7jsGbZ0E@cluster0.zvkwjl0.mongodb.net/travelBudgetApp',)
 
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connect
